d4e11a62951bd8326cabde99699c57740e0a4fef
Dubabbi <thdms3160@naver.com>
2025-02-16 01:28:42 +0900

    remove: 프록시 설정 제거

--- src/services/axiosInstance.js
+++ src/services/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "/api",
+  baseURL: "http://54.180.113.3:3000",
   timeout: 10000,
   headers: {
     "Content-Type": "application/json",
